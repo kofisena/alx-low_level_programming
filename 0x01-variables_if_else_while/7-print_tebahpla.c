9e0e4f5a3033fa8f7bada115e49d9027ae361378
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 13:15:04 -0800

    this

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.d>
+/**
+ * main - The beginning
+ * Return: This wibb be 0
+ */
+
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
