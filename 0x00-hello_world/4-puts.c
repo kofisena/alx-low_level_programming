4d43d4c873b1f8a332ffdd5984bd1e87c53d73be
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 05:46:14 -0800

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This is the first page of the program
+ * Return int
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
