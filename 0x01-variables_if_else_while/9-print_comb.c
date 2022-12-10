16d53969535d195683cd48601fa84efaeb4f5e8f
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 14:02:45 -0800

    last

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
-		if (a < 9){
+		if (a < 9)
+		{
 			putchar(',');
 			putchar(',');
 		}
 	}
 	putchar('\n');
-			
+
 	return (0);
 }
