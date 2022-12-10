b657576c2557e09d1392ea5b0f0c014173c3f856
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 12:11:34 -0800

    This is number 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the beggining of the program
+ * Return: This will return 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
