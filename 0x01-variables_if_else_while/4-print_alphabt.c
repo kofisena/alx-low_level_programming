3fc2984435d4879bbd0ea85cd5d976e362e69815
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 12:35:12 -0800

    something

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main entry point of my program
+ * Return: This is 0
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; a++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
