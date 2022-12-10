ba6ad50f8b772b67c709fc9c6d965d8d1f2a0614
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 13:26:16 -0800

    almost final

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include< stdio.h>
+/**
+ * main - This is the entry point
+ * Return: This will return 0
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
