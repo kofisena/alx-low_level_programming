a38c7e10b66ffd5879e0434e2add950a3593c5cc
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-10 06:23:45 -0800

    the last one

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is the entry to my program
+ * Return: This will return 0
+ */
+
+int main(void)
+{
+	printf("The size of a charater is %i bytes\n", sizeof(char));
+	printf("The size of integer ia %i bytes\n", sizeof(int));
+	printf("The size of long int is %i bytes\n", sizeof(long int));
+	printf("The size of a float is %i bytes\n", sizeof(float));
+	return (0);
+}
