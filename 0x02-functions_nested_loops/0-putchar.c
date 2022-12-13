5c0097f383261c3ead48c1884fc395ddc5d31cdd
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-13 11:58:46 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry
  * Return: 0
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t';
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
 
 	return (0);
 }
