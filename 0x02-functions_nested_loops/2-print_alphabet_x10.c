2284aad4cf12dbf964fd3edee7f28556e28c15d1
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-13 13:56:33 -0800

    hii

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,13 @@
  */
 void print_alphabet_x10(void)
 {
+	int count = 0;
 	char lett;
 
-	for lett = 'a'; lett <= 'z'; lett++
-		_putchar(lett);
-	_putchar('/n');
+	while (count++ <= 9)
+	{
+		for (lett = 'a'; lett <= 'z'; lett++)
+			_putchar(lett);
+		_putchar('/n');
+	}
 }
