0271798ee54508cccc3e12221a5cca55b0c9f349
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-14 12:35:22 -0800

    m

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,10 +11,10 @@ int i;
 printf("infinit loop incoming :(\n");
 i = 0;
 /*
- * while (i < 10)
- * {
+ *while (i < 10)
+ *{
  *	putchar(i);
- * }
+ *}
  */
 
 	printf("Infinite loop avoided! \\o/\n");
