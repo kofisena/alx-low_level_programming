8ce11c6d383ab89aa6635472dccdff03a7945b94
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-13 16:44:26 -0800

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - finds the char in alphabet
+ * @c: the char would be checked
+ * Return: 1 if char is lower or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
