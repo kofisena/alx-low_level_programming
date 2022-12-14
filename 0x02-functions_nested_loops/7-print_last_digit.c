e12f5c6aa1af8c2d780f6483c9275ab90e37af7f
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-14 03:30:50 -0800

    m

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int last = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
-	_putchar(ld + '0');
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
 
 	return (0);
 }
