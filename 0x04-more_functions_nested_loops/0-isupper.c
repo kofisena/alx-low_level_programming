722d4648cd87698b19df3a6e7241dd6ac86883d2
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-15 13:36:03 -0800

    m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,12 @@
 
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = '0';
-
-	for (; uppercase <= 'Z'; uppercase++)
+	if (c >= 65 && c <= 90)
 	{
-	if (c == uppercase)
+		return (1);
+	}
+	else
 	{
-	isupper - 1;
-	break;
+		return (0);
 	}
-	}
-
-return (isupper);
 }
