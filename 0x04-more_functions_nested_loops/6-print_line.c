c0f6c9dd97f785fcdd75bb68452e6f81f3550446
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-16 02:39:45 -0800

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - printing of lines
+ * @n: number of the lines
+ *
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+{_putchar('_');
+}
+
+_putchar('\n');
+}
