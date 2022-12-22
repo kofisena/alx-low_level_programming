7c5809444ae581ce7e16b4363f445b43eddc96e7
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-22 10:51:48 -0800

    m

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncp - this will copy a string
+ * @dest: dest input 
+ * @src: src input
+ * @n: n input
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+
+}
