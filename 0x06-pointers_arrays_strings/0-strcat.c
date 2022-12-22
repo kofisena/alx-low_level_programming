fc0ce99f50fdf53980a4ceda773be42b0286e8bd
Bernard Asigbe <kofi.sena@gmail.com>
2022-12-22 01:46:00 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  * Return: The pointer to the @dest
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
@@ -16,6 +16,7 @@ char *strcat(char *dest, const char *src)
 		dest_len++;
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
 
 }
