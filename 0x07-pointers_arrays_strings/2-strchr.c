d737ef7585f02b01be8f5f035341e9baa25cec47
Bernard Asigbe <kofi.sena@gmail.com>
2023-01-04 18:11:12 -0800

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: 0
+ */
+char *_strchr(char *, char c)
+{
+	int i = 0;
+
+	for (; s[i] == c)
+	{
+		return (&s[i]);
+	}
+	return (0);
+}
