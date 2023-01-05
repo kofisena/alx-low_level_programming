5297755b9bf6936cf4703098930099e5b09ece3d
Bernard Asigbe <kofi.sena@gmail.com>
2023-01-04 17:03:18 -0800

    0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * description: Header file containing prototype
- */
-
-#include <stddef.h>
-
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+void *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
