d989a5497e49cce8cfc563a2231b2133f2f8d6b4
Bernard Asigbe <kofi.sena@gmail.com>
2023-01-04 17:19:47 -0800

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,21 +3,19 @@
 /**
  * _memset - First n bytes of memory space which is poini to by @s and @c
  * @s: A pointer to the memory area to be filled
- * @c: The char to fill the memory space
+ * @b: The char to fill the memory space
  * @n: The number of bytes used
- * description _memset:something
  * Return: A pointer to the filled memory space @s
  */
 
 void *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-	
+
 	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
-	
 	return (s);
 }
