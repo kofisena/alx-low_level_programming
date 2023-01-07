9d1ffcce7ec47d5ecb4ce4dcae62fef5fbbd366b
Bernard Asigbe <kofi.sena@gmail.com>
2023-01-06 17:53:55 -0800

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,10 @@
 
 int factorial(int n)
 {
-	int result = n;
-
-	if (n < 0)
-		return (-1);
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
+	else if (n < 0)
+		return (-1);
 	else
-		return (0);
+		return (n * factorial(n - 1));
 }
