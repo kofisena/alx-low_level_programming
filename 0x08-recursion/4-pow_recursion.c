dcceb4d3e053a2c07fc50018d7466cfd3bd41753
Bernard Asigbe <kofi.sena@gmail.com>
2023-01-06 18:27:19 -0800

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - output the value of x to the power
+ * @x: the base number
+ * @y: the power number
+ * Return: power recursio
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+}
