f2ba957065f4d8b523a831ea623c6a39a81f670e
Bernard Asigbe <kofi.sena@gmail.com>
2023-05-18 16:57:38 -0700

    6-sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Size of an int: %u byte(s)", sizeof(int));
 	printf("Size of a long int: %u byte(s)", sizeof(long int);
 	printf("Size of a long long int: %u byte(s)", sizeof(long long int));
-	printf("Size of a float: %u byte(s), sizeof(float);
-	
+	printf("Size of a float: %u byte(s)", sizeof(float);
+
 	return (0);
 }
