0b3bfdd6033a1740fa30813038bce610247e661c
Bernard Asigbe <kofi.sena@gmail.com>
2023-05-21 17:17:44 -0700

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#inlude <stdio.h>
+/**
+ * main - Entry to program
+ * Return: 
+ */
+
+int main()
+{
+	char i;
+	for(i = 'a'; i<= 'z'; i++)
+	{
+	putchar(i);
+	putchar('\n');
+	}
+
+	return (0);
+}
