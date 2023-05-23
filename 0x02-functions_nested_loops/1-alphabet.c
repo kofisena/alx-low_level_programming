0b55d7c5dcdce3adefdfdf2b38daf4e8842c1c83
Bernard Asigbe <kofi.sena@gmail.com>
2023-05-23 15:36:40 -0700

    1-alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this will print alpha to stdout
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char aCharacter;
+
+	for (aCharacter = 'a' aCharacter <= 'z'; aCharacter++)
+	{
+	_putchar(aCharacter);
+
+	}
+	return (0);
+}
